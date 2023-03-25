eb5e074d5a3dfca59583235011e60d3524abece4
Yutaka Catch <yyycatch@gmail.com>
2023-03-25 20:51:09 +0900

    Game direction was changed from horizontal to vertical.

--- pong.js
+++ pong.js
@@ -1,14 +1,14 @@
 // JS BreakeOut Game by ChatGPT and Me
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-const paddleHeight = 100, paddleWidth = 10, ballSize = 10;
-let paddleY = (canvas.height - paddleHeight) / 2, ballX = canvas.width / 2, ballY = canvas.height / 2, ballVelX = -5, ballVelY = 5;
+const paddleWidth = 100, paddleHeight = 10, ballSize = 10;
+let paddleX = (canvas.width - paddleWidth) / 2, ballX = canvas.width / 2, ballY = canvas.height / 2, ballVelX = 5, ballVelY = -5;
 let gamePaused = true;
 
 canvas.addEventListener('mousemove', (e) => {
-  const relativeY = e.clientY - canvas.getBoundingClientRect().top;
-  if (relativeY - paddleHeight / 2 > 0 && relativeY + paddleHeight / 2 < canvas.height) {
-    paddleY = relativeY - paddleHeight / 2;
+  const relativeX = e.clientX - canvas.getBoundingClientRect().left;
+  if (relativeX - paddleWidth / 2 > 0 && relativeX + paddleWidth / 2 < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
   }
 });
 
@@ -29,34 +29,34 @@ function displayGameOver() {
 function resetGame() {
   ballX = canvas.width / 2;
   ballY = canvas.height / 2;
-  ballVelX = -5;
-  ballVelY = 5;
+  ballVelX = 5;
+  ballVelY = -5;
 }
 
 function gameLoop() {
-    if (gamePaused) return;
-  
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-    // Draw paddle and ball
-    ctx.fillStyle = 'white';
-    ctx.fillRect(canvas.width - paddleWidth, paddleY, paddleWidth, paddleHeight);
-    ctx.fillRect(ballX - ballSize / 2, ballY - ballSize / 2, ballSize, ballSize);
-  
-    // Update ball position and handle collisions
-    ballX += ballVelX;
-    ballY += ballVelY;
-  
-    if (ballY <= 0 || ballY + ballSize / 2 >= canvas.height) ballVelY = -ballVelY;
-    if (ballX <= ballSize / 2) {
-      ballVelX = -ballVelX;
-    } else if (ballX + ballSize / 2 >= canvas.width) {
-      displayGameOver();
-      gamePaused = true;
-      return;
-    } else if (ballX + ballSize / 2 >= canvas.width - paddleWidth - 1 && ballY >= paddleY - ballSize / 2 && ballY <= paddleY + paddleHeight + ballSize / 2) {
-      ballVelX = -ballVelX;
-    }
-  
-    requestAnimationFrame(gameLoop);
+  if (gamePaused) return;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw paddle and ball
+  ctx.fillStyle = 'white';
+  ctx.fillRect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillRect(ballX - ballSize / 2, ballY - ballSize / 2, ballSize, ballSize);
+
+  // Update ball position and handle collisions
+  ballX += ballVelX;
+  ballY += ballVelY;
+
+  if (ballX <= 0 || ballX + ballSize / 2 >= canvas.width) ballVelX = -ballVelX;
+  if (ballY <= ballSize / 2) {
+    ballVelY = -ballVelY;
+  } else if (ballY + ballSize / 2 >= canvas.height - paddleHeight - 1 && ballX >= paddleX - ballSize / 2 && ballX <= paddleX + paddleWidth + ballSize / 2) {
+    ballVelY = -ballVelY;
+  } else if (ballY + ballSize / 2 >= canvas.height) {
+    displayGameOver();
+    gamePaused = true;
+    return;
+  }
+
+  requestAnimationFrame(gameLoop);
 }
